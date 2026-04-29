2b70eb7850ebfd0bacf2f6b551f148bb64c8c08d
ayhamrabea <ayhamrabea05@gmail.com>
2026-04-29 21:21:02 +0300

    add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Script from 'next/script'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -26,6 +28,25 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ru">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-66878ZGQLN`}
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-66878ZGQLN');
+            `,
+          }}
+        />
+      </head>
       <body className={`${inter.className} bg-gray-50 min-h-screen`}>
         {children}
       </body>
